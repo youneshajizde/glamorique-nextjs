48391a52bc10071e773e7ec28f83fab80d7f36ff
Jonas <jonaselemented@gmail.com>
2024-06-13 19:50:08 -0700

    home page has been completed

--- components/Product.jsx
+++ components/Product.jsx
@@ -12,7 +12,7 @@ function Product({ name, brand, img, btn }) {
   };
 
   return (
-    <div className="relative w-[200px]  rounded-lg mt-4 shadow-lg">
+    <div className="relative w-[200px]  rounded-lg mt-4 ">
       <span
         className="absolute w-8 h-8 bg-white p-1 shadow-md top-2 left-2 rounded-lg flex items-center justify-center cursor-pointer"
         onClick={toggleHeart}
@@ -33,7 +33,7 @@ function Product({ name, brand, img, btn }) {
       <div className="flex flex-col items-center">
         <h3 className="font-medium text-sm">{name}</h3>
         <span className="text-xs font-medium text-gray-500">{brand}</span>
-        <button className="text-xs text-gray-500 border-2 rounded-lg py-1 px-2 mt-3">
+        <button className="text-xs text-gray-500 border-2 rounded-lg py-1 px-2 mt-3 hover:bg-slate-900 hover:text-white">
           {btn}
         </button>
       </div>
